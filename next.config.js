c7254f043b0f1455d9f737b988e9917adfb3e8e5
Paddy Keane <Paddy.Keane@akqa.com>
2019-01-09 15:41:26 +0000

    copying cloudinary creds from env vars into netlifycms config.yml at build time

--- next.config.js
+++ next.config.js
@@ -2,9 +2,22 @@ const fs = require('fs-extra')
 const { join } = require('path')
 const { promisify } = require('util')
 const copyFolder = promisify(fs.copy)
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const rename = promisify(fs.rename)
 
 module.exports = {
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+
+    let contents = await readFile(join(dir, 'admin/config.yml'), 'utf8')
+    let replaced_contents = contents
+      .replace('${CLOUDINARY_CLOUD_NAME}', env.CLOUDINARY_CLOUD_NAME)
+      .replace('${CLOUDINARY_API_KEY}', env.CLOUDINARY_API_KEY)
+
+    let tmpfile = `${file}.jstmpreplace`
+    await writeFile(tmpfile, replaced_contents, 'utf8')
+    await rename(tmpfile, file)
+
     await copyFolder(join(dir, 'admin'), join(outDir, 'admin'))
     return defaultPathMap
   },
