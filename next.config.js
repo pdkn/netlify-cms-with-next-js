2f35db27c2aab43bc3ff5afdf0140a679d3ce319
Paddy Keane <Paddy.Keane@akqa.com>
2019-01-09 15:54:58 +0000

    copying cloudinary creds from env vars into netlifycms config.yml at build time

--- next.config.js
+++ next.config.js
@@ -6,19 +6,24 @@ const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 const rename = promisify(fs.rename)
 
+require('dotenv').config({ debug: process.env.DEBUG })
+
 module.exports = {
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
 
-    let contents = await readFile(join(dir, 'admin/config.yml'), 'utf8')
-    let replaced_contents = contents
-      .replace('${CLOUDINARY_CLOUD_NAME}', env.CLOUDINARY_CLOUD_NAME)
-      .replace('${CLOUDINARY_API_KEY}', env.CLOUDINARY_API_KEY)
-
-    let tmpfile = `${file}.jstmpreplace`
-    await writeFile(tmpfile, replaced_contents, 'utf8')
-    await rename(tmpfile, file)
-
     await copyFolder(join(dir, 'admin'), join(outDir, 'admin'))
+
+    let cmsConfigFile = join(outDir, 'admin/config.yml')
+    let contents = await readFile(cmsConfigFile, 'utf8')
+    let replaced_contents = contents
+      .replace('${CLOUDINARY_CLOUD_NAME}', process.env.CLOUDINARY_CLOUD_NAME)
+      .replace('${CLOUDINARY_API_KEY}', process.env.CLOUDINARY_API_KEY)
+
+    let tmpfile = `${cmsConfigFile}.jstmpreplace`
+    await writeFile(tmpfile, replaced_contents, 'utf8')
+    await rename(tmpfile, cmsConfigFile)
+
+
     return defaultPathMap
   },
 };
