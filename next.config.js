d7835938208a0cc6ed59ae31210ec14b9bec2a77
Paddy Keane <Paddy.Keane@akqa.com>
2019-01-09 14:39:14 +0000

    copy root 'admin' folder into static export 'out' folder

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const fs = require('fs-extra')
+const { join } = require('path')
+const { promisify } = require('util')
+const copyFolder = promisify(fs.copy)
+
 module.exports = {
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      '/': { page: '/' },
-      '/admin': { page: '/admin' },
-    };
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    await copyFolder(join(dir, 'admin'), join(outDir, 'admin'))
+    return defaultPathMap
   },
 };
