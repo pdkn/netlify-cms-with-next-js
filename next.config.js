f46f90663d1733df678fa8ed66bb01cd4a4959eb
Paddy Keane <Paddy.Keane@akqa.com>
2019-01-09 17:10:21 +0000

    preventing access to cms config.yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
 const { join } = require('path')
 const { promisify } = require('util')
+const copyFile = promisify(fs.copyFile)
 const copyFolder = promisify(fs.copy)
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
@@ -19,10 +20,20 @@ module.exports = {
       .replace('${CLOUDINARY_CLOUD_NAME}', process.env.CLOUDINARY_CLOUD_NAME)
       .replace('${CLOUDINARY_API_KEY}', process.env.CLOUDINARY_API_KEY)
 
-    let tmpfile = `${cmsConfigFile}.jstmpreplace`
-    await writeFile(tmpfile, replaced_contents, 'utf8')
-    await rename(tmpfile, cmsConfigFile)
+    let tmpCmsConfigFile = `${cmsConfigFile}.jstmpreplace`
+    await writeFile(tmpCmsConfigFile, replaced_contents, 'utf8')
+    await rename(tmpCmsConfigFile, cmsConfigFile)
 
+    let headersFile = join(outDir, '_headers')
+    await copyFile(join(dir, '_headers'), headersFile)
+    let headersContent = await readFile(headersFile, 'utf8')
+    let replaced_headersContent = headersContent
+      .replace('${ADMIN_USERNAME}', process.env.ADMIN_USERNAME)
+      .replace('${ADMIN_PASSWORD}', process.env.ADMIN_PASSWORD)
+
+    let tmpHeadersFile = `${headersFile}.jstmpreplace`
+    await writeFile(tmpHeadersFile, replaced_headersContent, 'utf8')
+    await rename(tmpHeadersFile, headersFile)
 
     return defaultPathMap
   },
